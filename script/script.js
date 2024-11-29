80c4c3285649e29cae7236e0fbad9415bcda014c
rococorbalan <123517270+rococorbalan@users.noreply.github.com>
2024-11-28 22:47:23 -0300

    Add grid display and change to game flow
    Add grid with cells that handle the clicks, on
    every click checks who is the current player and
    if win condition is true then current player wins.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const board = (function () {
     const init = () => {
-        let gameboard = ["*", "*", "*", "*", "*", "*", "*", "*", "*"];
+        let gameboard = ["", "", "", "", "", "", "", "", ""];
 
         gameboard.display = () => {
             console.log(gameboard[0], gameboard[1], gameboard[2]);
@@ -13,11 +13,11 @@ const board = (function () {
         };
 
         gameboard.isFull = () => {
-            return (gameboard.every(cell => cell !== "*"));
+            return (gameboard.every(cell => cell !== ""));
         };
 
         gameboard.isEmpty = () => {
-            return (gameboard.every(cell => cell === "*"));
+            return (gameboard.every(cell => cell === ""));
         }
 
         gameboard.winCondition = () => {
@@ -27,25 +27,25 @@ const board = (function () {
         
             // Check horizontals
             for(let i = 0; i <= 6; i += 3){
-                if((gameboard[i] === gameboard[i+1] && gameboard[i] === gameboard[i+2]) && gameboard[i] !== "*"){
+                if((gameboard[i] === gameboard[i+1] && gameboard[i] === gameboard[i+2]) && gameboard[i] !== ""){
                     return true;
                 }
             }
         
             //Check verticals
             for(let i = 0; i <= 2; i++){
-                if((gameboard[i] === gameboard[i+3] && gameboard[i] === gameboard[i+6]) && gameboard[i] !== "*"){
+                if((gameboard[i] === gameboard[i+3] && gameboard[i] === gameboard[i+6]) && gameboard[i] !== ""){
                     return true;
                 }
             }
         
             //Check diagonal top to bottom
-            if((gameboard[0] === gameboard[4] && gameboard[0] === gameboard[8]) && gameboard[0] !== "*"){
+            if((gameboard[0] === gameboard[4] && gameboard[0] === gameboard[8]) && gameboard[0] !== ""){
                 return true;
             }
         
             //Check diagonal bottom to top
-            if((gameboard[6] === gameboard[4] && gameboard[6] === gameboard[2]) && gameboard[6] !== "*"){
+            if((gameboard[6] === gameboard[4] && gameboard[6] === gameboard[2]) && gameboard[6] !== ""){
                 return true;
             }
         
@@ -61,6 +61,30 @@ const board = (function () {
 let gameboard = board.init();
 gameboard.display();
 
+
+const displayController = (function () {
+    let cells = document.querySelectorAll(".cell");
+    const getCells = () => cells
+
+    const displayBoard = (board) => {
+        for(let i = 0; i < board.length; i++){
+            cells[i].textContent = board[i];
+        }
+    }
+
+    cells.forEach(cell => cell.addEventListener("click", (event) => {
+        let currentCell = event.currentTarget.classList[1]
+        currentPlayer.move(gameboard, currentCell);
+        displayController.displayBoard(gameboard);
+
+        gameFlow.handleTurn(gameboard, player1, player2);
+        
+    }))
+
+    return { getCells, displayBoard };
+})();
+
+
 function createPlayer(name, playerNumber) {
     let playerIcon;
     if (playerNumber === 1){
@@ -69,19 +93,8 @@ function createPlayer(name, playerNumber) {
         playerIcon = "O";
     }
 
-    let move = (gameboard) => {
-        let moveInput = parseInt(prompt(`${name}'s turn!`));
-        if (moveInput === 2907) {
-            return;
-        }
-        if((moveInput < 0 || moveInput > 8) || isNaN(moveInput)){
-            alert("Choose a cell between 0 and 8");
-            return move(gameboard);
-        }else if(!gameboard.checkCell(moveInput)){
-            alert("This cell is occupied!");
-            return move(gameboard);
-        }
-        gameboard[moveInput] = playerIcon;
+    let move = (gameboard, cell) => {
+        gameboard[cell] = playerIcon;
     }
 
     const getPlayerIcon = () => playerIcon;
@@ -89,47 +102,41 @@ function createPlayer(name, playerNumber) {
 }
 
 // Delete this when you finish lol
-const roco = createPlayer("roco", 1);
-const roca = createPlayer("roca", 2);
+const player1 = createPlayer("roco", 1);
+const player2 = createPlayer("roca", 2);
+let currentPlayer = player1;
+function toggleCurrentPlayer () {
+    if (currentPlayer === player1) {
+        currentPlayer = player2;
+    }else {
+        currentPlayer = player1;
+    }
+}
 
 
 const gameFlow = (function () {
-    const startGame = function(gameboard, player1, player2){
-        const { name: player1Name } = player1;
-        const { name: player2Name } = player2;
-
-        let player1Turn = true;
-
-        // Resets the gameboard
-        gameboard = board.init();
-
-        while(!gameboard.isFull()){
-            if(player1Turn) {
-                player1.move(gameboard);
-                console.clear()
-                gameboard.display();
-                if(gameboard.winCondition()){
-                    console.log(`${player1Name} won!`);
-                    break;
-                }
-                player1Turn = false;
-            }else {
-                player2.move(gameboard);
-                console.clear();
-                gameboard.display();
-                if(gameboard.winCondition()){
-                    console.log(`${player2Name} won!`);
-                    break;
-                }
-                player1Turn = true;
+    const handleTurn = (gameboard, player1, player2) =>{
+        if(gameboard.isFull()){
+            if(gameboard.winCondition() === false){
+                console.log("tie");
             }
         }
-        if(gameboard.winCondition() === false){
-            console.log("is a tie!")
+        if(currentPlayer === player1){
+            if(gameboard.winCondition()) {
+                console.log("player 1 wins");
+            }else {
+                toggleCurrentPlayer();
+            }
+        }else {
+            if(gameboard.winCondition()) {
+                console.log("player 2 wins");
+            }else {
+                toggleCurrentPlayer();
+            }
         }
     }
 
-    return { startGame };
+    return { handleTurn };
 })();
 
 
